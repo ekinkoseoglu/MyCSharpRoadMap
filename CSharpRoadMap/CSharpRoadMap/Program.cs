1e6b5ebb4063e9d7a7beebebdec17b8e2b4a142d
Ekin Deniz Koseoglu <78482939+ekinkoseoglu@users.noreply.github.com>
2021-03-23 16:44:12 +0300

    Update Program.cs

--- CSharpRoadMap/CSharpRoadMap/Program.cs
+++ CSharpRoadMap/CSharpRoadMap/Program.cs
@@ -6,7 +6,13 @@ namespace CSharpRoadMap
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           // Hepinize merhabalar ben Ekin, 23 yaşındayım ve C# programlamaya 2 ay önce başladım. 
+            //Eğitimim ve yolculuğum boyunca her gün öğrendiğim yeni şeyleri sizinle paylaşmak ve Github platformunu daha aktif ve doğru kullanabilmeyi anlamak adına paylaşım yapıyorum
+            // amacım benimle birlikte yeni başlamış insanlar bularak onların da benim kaçırdığım ya da kendilerinin de önemli gördüğü başka konularda bu Repository'e ekleme yapması ve
+            //Bu RoadMap'i yeni başlayacak insanlar adına kolay ve anlaşılır bir rehber haline getirmektir.
+            
+            // Benim gözümden kaçan veya sizin özellikle eklemekm istediğiniz bölümleri Request göndererek ekleyebilirsiniz lütfen çekinmeyin.
+            // Değerli vaktinizi harcadığınız için teşekkürler.
         }
     }
 }
