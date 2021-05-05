2767bc552465aa5afcce7d075267f14589618d0c
Ekin Deniz Koseoglu <ekindenizkoseoglu@gmail.com>
2021-05-05 12:51:45 +0300

    05.05.2021
    Added new EventsAndDelegates Project

---
+++ CSharpRoadMap/Delegates/EventsAndDelegates/MailService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EventsAndDelegates
+{
+    public class MailService // This class responsible for sending Email once the Video has encoded
+    {
+        public void OnVideoEncoded(object source, VideoEventArgs e) //Create publisher method of event (but not subscripted yet...)
+        {
+            Console.WriteLine("MailService: Sending an email..." + e.Video.Title);
+        }
+    }
+}
